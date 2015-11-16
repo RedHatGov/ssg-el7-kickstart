de2df06c6420aaa3a00f65f31a3f2b066667b7bf
Frank Caviggia <fcaviggia@users.noreply.github.com>
2015-11-16 09:28:35 -0500

    Update ipa-pam-configuration.sh
    Updates to PAM configuration - added pam_systemd.so and removed /etc/pam.d/gnome-screensaver (incorporated into GDM now)

--- config/hardening/ipa-pam-configuration.sh
+++ config/hardening/ipa-pam-configuration.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script was written by Frank Caviggia, Red Hat Consulting
-# Last update was 12 Nov 2015
+# Last update was 16 Nov 2015
 # This script is NOT SUPPORTED by Red Hat Global Support Services.
 # Please contact Rick Tavares for more information.
 #
@@ -16,9 +16,6 @@ if [ ! -e /etc/pam.d/system-auth-local.orig ]; then
 fi
 if [ ! -e /etc/pam.d/password-auth-local.orig ]; then
   cp /etc/pam.d/password-auth-local /etc/pam.d/password-auth-local.orig
-fi
-if [ ! -e /etc/pam.d/gnome-screensaver.orig ]; then
-  cp /etc/pam.d/gnome-screensaver /etc/pam.d/gnome-screensaver.orig
 fi
 
 # Deploy Configuruation
@@ -51,6 +48,7 @@ password required pam_deny.so
 session required pam_lastlog.so showfailed
 session optional pam_keyinit.so revoke
 session required pam_limits.so
+-session optional pam_systemd.so
 session optional pam_oddjob_mkhomedir.so umask=0077
 session [success=1 default=ignore] pam_succeed_if.so service in crond quiet use_uid
 session required pam_unix.so
@@ -88,47 +86,12 @@ password required pam_deny.so
 session required pam_lastlog.so showfailed
 session optional pam_keyinit.so revoke
 session required pam_limits.so
+-session optional pam_systemd.so
 session optional pam_oddjob_mkhomedir.so umask=0077
 session [success=1 default=ignore] pam_succeed_if.so service in crond quiet use_uid
 session required pam_unix.so
 session optional pam_sss.so
 EOF
 ln -sf /etc/pam.d/password-auth-local /etc/pam.d/password-auth
-
-cat <<EOF > /etc/pam.d/gnome-screensaver
-%PAM-1.0
-auth [success=done ignore=ignore default=bad] pam_selinux_permit.so
-auth required pam_env.so
-auth required pam_lastlog.so
-auth required pam_faillock.so preauth silent audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth sufficient pam_unix.so try_first_pass
-auth sufficient pam_sss.so try_first_pass
-auth [default=die] pam_faillock.so authfail audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth sufficient pam_faillock.so authsucc audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth requisite pam_succeed_if.so uid >= 1000 quiet
-auth required pam_deny.so
-auth optional pam_gnome_keyring.so
-
-account required pam_faillock.so
-account required pam_unix.so
-account required pam_lastlog.so
-account sufficient pam_localuser.so
-account sufficient pam_succeed_if.so uid < 1000 quiet
-account [default=bad success=ok user_unknown=ignore] pam_sss.so
-account required pam_permit.so
-
-# Password Quality now set in /etc/security/pwquality.conf
-password requried pam_pwqaulity.so retry=3
-password sufficient pam_unix.so sha512 shadow try_first_pass use_authtok remember=24
-password sufficient pam_sss.so use_authtok
-password required pam_deny.so
-
-session required pam_lastlog.so showfailed
-session optional pam_keyinit.so revoke
-session required pam_limits.so
-session [success=1 default=ignore] pam_succeed_if.so service in crond quiet use_uid
-session required pam_unix.so
-session optional pam_sss.so
-EOF
 
 exit 0
