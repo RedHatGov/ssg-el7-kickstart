aa6cf8ae2d9dbc7ebb29dff03bc3ce688e6a7285
Frank Caviggia <fcaviggia@users.noreply.github.com>
2016-12-18 08:14:10 -0500

    UMASK update
    Privileged users get 022 when acting as root, non-privileged users get 027 only with bash.

--- config/hardening/ssg-supplemental.sh
+++ config/hardening/ssg-supplemental.sh
@@ -385,12 +385,19 @@ chmod 755 /etc/profile.d/autologout.sh
 chmod 755 /etc/profile.d/autologout.csh
 
 
-########################################
+#########################################
 # Set Shell UMASK Setting (027)
 ########################################
 cat <<EOF > /etc/profile.d/umask.sh
 #!/bin/sh
-umask 027
+
+# Non-Privledged Users get 027
+# Privledged Users get 022
+if [[ $EUID -ne 0 ]]; then
+	umask 027
+else
+	umask 022
+fi
 EOF
 cat <<EOF > /etc/profile.d/umask.csh
 #!/bin/csh
